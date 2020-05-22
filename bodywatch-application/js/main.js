de5a529b75cd81fb087776b720fbf16da1438fe8
Pranawa Somvedi <pranawab@gmail.com>
2020-05-22 13:21:06 +0200

    Update main.js
    - Added functionalities to complete application. Ready to push to master

--- bodywatch-application/js/main.js
+++ bodywatch-application/js/main.js
@@ -1,21 +1,12 @@
-window.onload = () => {
-    'use strict';
-    
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('./sw.js');
-    }
-}
-
 let video;
 let poseNet;
-//let poses = [];
 let pose;
 let options = {
     architecture: 'MobileNetV1',
     imageScaleFactor: 0.3,
     outputStride: 16,
     flipHorizontal: false,
-    minConfidence: 0.5,
+    minConfidence: 1,
     maxPoseDetections: 1,
     scoreThreshold: 0.5,
     nmsRadius: 20,
@@ -41,10 +32,11 @@ let frequentTime = 0.25; //voor nu staat hij op 0.25 om te testen maar in produc
 let goodPoseTime = 1; //voor nu staat hij op 1 om te testen maar in productie zou hij bijvoorbeeld elke uur kunnen aangeven of je fout heb gezeten
 let badpose_per_session = [];
 let badposeCounter_per_session = 0;
+let used = false;
 
 function setup() {
     //create camera window and webcam usage.
-    var test = createCanvas(800, 600);
+    var test = createCanvas(640, 480);
     test.parent('canvasPosition'); //dit koppelt het aan een div in html 
     test.position(400,120); //relocate canvas
     video = createCapture(VIDEO);
@@ -52,9 +44,6 @@ function setup() {
     poseNet = ml5.poseNet(video, options, check);
     
     poseNet.on('pose', showPoses)
-
-    
-    
     video.hide();
 
     //functies hier uitvoeren zorgt misschien voor beetje lag? weet niet zeker even testen 
@@ -70,7 +59,7 @@ function check() {
 
 function showPoses(poses) {
     //show pose x and y variables.
-    console.log(poses);
+    // console.log(poses);
     if (poses.length > 0) {
         pose = poses[0].pose;
         skeleton = poses[0].skeleton;
@@ -164,6 +153,18 @@ function randomNotifications() {
                     }
                 });
             }, (frequentTime * 2) * 60 * 1000);
+
+        setTimeout(
+            function() {
+                Push.create('Time for a break!', {
+                    body: "Click on this notification to take a break. x minutes have passed.",
+                    onClick: function () {
+                        window.location.href = "./timer/timer_index.html"
+                        this.close();
+                    }
+                });
+            }, ((frequentTime * 3) * 60 * 1000)
+        );
 }
 
 function testHyperlink() {
@@ -325,28 +326,6 @@ function randomTips(){
         }, 2.0 * 60 * 1000); //staat op anderhalf minuut
 }
 
-//test code geschreven hoef NIET gebruikt te worden misschien in toekomst
-//dit checkt elke x seconden of er verandering is (in dit geval op variable d) 
-//dit kan in toekomst gebruikt worden als het nodig is eventueel voor nu geen behoefte 
-function checkChange(){
-    var someValue = d;
-    var prevVal = null;             
-    var _myInterval = setInterval(function() {
-        if(prevVal == someValue) {
-            console.log("No change for 2 second", someValue)
-            someValue = d;
-            //hier kan je iets uitvoeren als er 2 seconden niks veranderd is misschien reminder of timer 
-            //kunnen misschien in plaats van == het van elkaar aftrekken en met marges spelen want camera kan shaken door licht inval
-        } else {
-            console.log("Value was changed between past 2 second prev: ", prevVal, " New: ", someValue)
-            prevVal = someValue;
-            someValue = d;
-            //als values niet gelijk meer zijn dan veranderd de opgeslagen value in de nieuwe value maar 
-            //hij kijkt of values PRECIES hetzelfde zijn dus beter spelen met marges
-        }
-    }, 2000)
-}
-
 //functie zodat om de x minuten word gecheckt of de gebruiker in die tijd een foute pose heeft gehad
 function checkGoodPose(){
     setTimeout(
@@ -404,17 +383,12 @@ function playSound(filename){
 function draw() {
     //draw both keypoints and the skeleton for testing purposes.
     drawKeyPoints();
-    ////drawSkeleton();
 }
 
 function drawKeyPoints() {
     image(video, 0, 0);
         
     try {
-        //make keypoints for the point between the shoulders
-        ////let midX = shoulderL.position.x + (shoulderR.position.x - shoulderL.position.x) * 0.50;
-        ////let midY = shoulderL.position.y + (shoulderR.position.y - shoulderL.position.y) * 0.50;
-        
         //trying to check distance eyes
         let eyeR = pose.rightEye;
         let eyeL = pose.leftEye;
@@ -422,22 +396,9 @@ function drawKeyPoints() {
         
         //fill with color red and create ellipse to show the keypoints
         fill(255,0,0);
-        ////ellipse(midX, midY, 34);
 
         //use d to check distance instead of fixed variable
         ellipse(pose.nose.x, pose.nose.y, d);
-        ////ellipse(pose.leftShoulder.x, pose.leftShoulder.y, 30);
-        /////ellipse(pose.rightShoulder.x, pose.rightShoulder.y, 30);
-
-        // // console.log(d);
-        // // console.log(leanCheck);
-        // // console.log(muteCheck);
-        // // console.log("reward good pose " + reward_good_pose);
-        // // console.log(frequentTime);
-        // // console.log(goodPoseTime);
-        console.log(badposeCounter_per_session);
-        console.log(badpose_per_session);
-        console.log(printBadSession());
 
         //if leaning forward && no notification is showing then show notification 
         //otherwise the notification will loop and crash the browser/application
@@ -457,66 +418,32 @@ function drawKeyPoints() {
     }
 }
 
-//function drawSkeleton() {
-//    try {
-//        //first 2 points in this array are the shoulders. 
-//        let a = skeleton[0][0];
-//        let b = skeleton[0][1];
-//        
-//        //put in global variable for drawKeyPoints
-//        shoulderL = a;
-//        shoulderR = b;
-//        
-//        //create middle keypoint for check later on
-//        let midX = shoulderL.position.x + (shoulderR.position.x - shoulderL.position.x) * 0.50;
-//        let midY = shoulderL.position.y + (shoulderR.position.y - shoulderL.position.y) * 0.50;
-//        
-//        strokeWeight(3);
-//        stroke(255);
-//        
-//        //draw lines
-//        line(a.position.x, a.position.y, b.position.x, b.position.y);  
-//        line(pose.nose.x, pose.nose.y, midX, midY);
-//    }
-//    catch(err) {
-//      //console.log("No pose found!");
-//    }   
-//}
+function personNotFound() {
+    if (used == false) {
+        console.log("Nobody is behind the camera.");
+        let timeout = setTimeout(function() { window.location.href ="./timer/timer_index.html" }, 10000);
+
+        Push.create('Are you still here?', {
+            body: "Click on this notification if you are here.",
+            onClick: function () {
+                clearTimeout(timeout);
+                console.log("Person back.");
+                used = false;
+                this.close();
+            }
+        });
+        used = true;
+    }    
+}
 
 function detectOutOfCanvas(){
     const nose = pose.nose;
     const leftShoulder = pose.leftShoulder;
     const rightShoulder = pose.rightShoulder;
-    console.log("x"+nose.x);
-    console.log("y"+nose.y);
+
     if (nose && nose.x && nose.y){
-        if (nose.x < 0 || nose.x >= 800){
-            console.log("nose x position is out of the image");
-        }
-    }
-    if (nose && nose.x && nose.y){
-        if (nose.y < 0 || nose.y >= 600){
-            console.log("nose y position is out of the image");
-        }
-    }
-    if (leftShoulder && leftShoulder.x && leftShoulder.y){
-        if (leftShoulder.y < 0 || leftShoulder.y >= 600){
-            console.log("leftShoulder y position is out of the image");
-        }
-    }
-    if (leftShoulder && leftShoulder.x && leftShoulder.y){
-        if (leftShoulder.x < 0 || leftShoulder.x >= 800){
-            console.log("leftShoulder x position is out of the image");
-        }
-    }
-    if (rightShoulder && rightShoulder.x && rightShoulder.y){
-        if (rightShoulder.x < 0 || rightShoulder.x >= 800){
-            console.log("rightShoulder x position is out of the image");
-        }
-    }
-    if (rightShoulder && rightShoulder.x && rightShoulder.y){
-        if (rightShoulder.y < 0 || rightShoulder.y >= 600){
-            console.log("rightShoulder y position is out of the image");
+        if (nose.x < 0 || nose.x >= 640){
+            personNotFound();
         }
     }
 }
